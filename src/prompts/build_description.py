c109fb0d8e5ccd75bef0774b3b35ef7e6e15fe4f
alexis.maes.ext <alexis.maes.ext@veolia.com>
2025-11-15 22:52:20 +0100

    upadate descriptions

---
+++ src/prompts/build_description.py
@@ -0,0 +1,14 @@
+BUILD_DESCRIPTION_PROMPT = """
+You are an expert un build professional descriptions of candidates for a job posting.
+The candidates are applying for technical position.
+Your task is to read the description of the applicant and write a professional description of the candidate.
+You must present 
+ - the strength and weakness of the candidates.
+ - It skills and how he used them in his previous experience.
+
+
+You must summerize the description in a maximum of 100 words:
+
+The description of the candidate is:
+{description}
+"""
