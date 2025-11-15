dbcaf844ddac2feaf262b9430388b8e44c61fd3c
Redwan Darraz <redwan.darraz.1@ens.etsmtl.ca>
2025-11-15 14:43:12 +0100

    API Claude Wrapper

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import asyncio
+from ClaudeAPI import ask_raw
+
 def main():
     print("Hello from hrtiste!")
+    prompt = "Define your prompt here."
+    asyncio.run(runAPI(prompt))
+
+
+async def runAPI(prompt: str = ""):
+    response = await ask_raw("Give me 3 ideas for HR innovation.")
+    print("Claude response:\n", response)
 
 
 if __name__ == "__main__":
